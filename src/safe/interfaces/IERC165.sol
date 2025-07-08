458f584070a4ace2794c634a58a32b709291fa01
Lawson Cheng <lawson.c@binance.com>
2025-07-08 16:36:04 +0800

    + new SafeGuard

--- src/safe/interfaces/IERC165.sol
+++ src/safe/interfaces/IERC165.sol
@@ -3,13 +3,13 @@ pragma solidity 0.8.24;
 
 /// @notice More details at https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/introspection/IERC165.sol
 interface IERC165 {
-    /**
-     * @dev Returns true if this contract implements the interface defined by `interfaceId`.
-     * See the corresponding EIP section
-     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified
-     * to learn more about how these ids are created.
-     *
-     * This function call must use less than 30 000 gas.
-     */
-    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+  /**
+   * @dev Returns true if this contract implements the interface defined by `interfaceId`.
+   * See the corresponding EIP section
+   * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified
+   * to learn more about how these ids are created.
+   *
+   * This function call must use less than 30 000 gas.
+   */
+  function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
